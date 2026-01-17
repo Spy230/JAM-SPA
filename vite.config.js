0b48d9497d7f1ad0ef99e7134a2c7f1ebc1f1492
Spy <shesterikovon@gmail.com>
2026-01-17 15:01:20 +0300

    Update styles and components

--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-        pure_funcs: ['console.log', 'console.info', 'console.debug'],
-        passes: 2
-      },
-      mangle: {
-        toplevel: true,
-        safari10: true
-      },
-      format: {
-        comments: false
-      }
-    },
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: undefined,
